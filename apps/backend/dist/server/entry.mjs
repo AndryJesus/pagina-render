ce298c47abc56aad47112ffe826369be7cff05d9
AndryJesus <andryjescobar@gmail.com>
2025-09-19 10:24:21 -0400

    cambios en el backend

--- apps/backend/dist/server/entry.mjs
+++ apps/backend/dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_BI3VR6e8.mjs';
-import { manifest } from './manifest_D9ucn9a9.mjs';
+import { manifest } from './manifest_CYPXR_AM.mjs';
 
 const serverIslandMap = new Map();;
 
